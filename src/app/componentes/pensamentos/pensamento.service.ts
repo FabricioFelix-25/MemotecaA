50ec24b93c675b2cc7fb8ca61882dda1d0c31656
Fabricio felix <felixfabricio115@gmail.com>
2025-03-06 20:15:35 -0300

    excluir e cancelar concluido

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Pensamento } from './pensamento';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,21 @@ constructor(private http: HttpClient) { }
 listar() {
   return this.http.get<Pensamento[]>(this.API);
 }
+
+
+criar (pensamento: Pensamento): Observable<Pensamento> {
+  return this.http.post<Pensamento>(this.API, pensamento);
+}
+
+excluir(id: number): Observable<Pensamento> {
+  const url = `${this.API}/${id}`
+  return this.http.delete<Pensamento>(url)
+}
+
+buscarPorId(id: number): Observable<Pensamento> {
+  const url = `${this.API}/${id}`
+  return this.http.get<Pensamento>(url)
+}
+
 
 }
